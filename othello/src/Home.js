2f5ce7767b71871141ef37ba1a9d08009fc92bec
unknown <andressc@arizona.edu>
2023-11-17 16:34:27 -0700

    Wrote up Help page

--- othello/src/Home.js
+++ othello/src/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom"
 
+const navigate = useNavigate();
+
 function getUser(){
 	let cookie = decodeURIComponent(document.cookie);
 	let currentUser;
