069c9a273950af7852092b5749f9679ea7ddfa32
ElijahCParent <eparent@arizona.edu>
2023-11-17 12:58:25 -0700

    Updates to login css

--- othello/src/Login.js
+++ othello/src/Login.js
@@ -31,19 +31,24 @@ function Login() {
         <div>
             <h1>Othello</h1>
 
-            <h1>Login</h1>
-            <InputPair name='username' value='Username' type='text' />
-            <InputPair name='password' value='Password' type='password' />
+            <div id='login_container'>
+                <div className='login_section'>
+                    <h1>Login</h1>
+                    <InputPair name='username' value='Username' type='text' />
+                    <InputPair name='password' value='Password' type='password' />
 
-            {/*TODO change onclick function to new login function*/}
-            <LoginButton id='login_button' /*onclick={() => attemptLogin()}*/ value='Log In' />
+                    {/*TODO change onclick function to new login function*/}
+                    <LoginButton id='login_button' /*onclick={() => attemptLogin()}*/ value='Log In' />
+                </div>
+                <div className='login_section'>
+                    <h1>Create Account</h1>
+                    <InputPair name='create_username' value='Username' type='text' />
+                    <PasswordPair name='create_password' value='Password' />
 
-            <h1>Create Account</h1>
-            <InputPair name='create_username' value='Username' type='text' />
-            <PasswordPair name='create_password' value='Password' />
-
-            {/*TODO change onclick function to new add user function*/}
-            <LoginButton id='add_user_button' /*onclick={() => addNewUser()}*/ value='Create' />
+                    {/*TODO change onclick function to new add user function*/}
+                    <LoginButton id='add_user_button' /*onclick={() => addNewUser()}*/ value='Create' />
+                </div>
+            </div>
         </div>
     );
 }
