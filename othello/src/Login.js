9edc32630d68056ed28c01ac4ff52362ac99e022
ElijahCParent <eparent@arizona.edu>
2023-11-17 12:33:59 -0700

    Rebuilt the login page to better follow React best practices.

--- othello/src/Login.js
+++ othello/src/Login.js
@@ -1,42 +1,49 @@
 import React from 'react';
 
+function InputPair({id, value, type}) {
+    return (
+        <div class='input_pair'>
+            <label for={id}>{value}</label>
+            <input type={type} name={id} id={id} class='inputs' pattern='.{8,}'
+                title='Eight or more characters' required />
+        </div>
+    );
+}
+
+function PasswordPair({id, value}) {
+    return (
+        <div class='input_pair'>
+            <label for={id}>{value}</label>
+            <input type='password' name={id} id={id} class='inputs' />
+        </div>
+    );
+}
+
+function LoginButton({id, onButtonClick, value}) {
+    return (
+        <div class='center_login_buttons'>
+            <button id={id} onclick={onButtonClick} class='submit_button'>{value}</button>
+        </div>
+    );
+}
 function Login() {
     return (
         <div>
             <h1>Othello</h1>
 
             <h1>Login</h1>
-            <div class="input_pair">
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" class="inputs" />
-            </div>
-
-            <div class="input_pair">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" class="inputs" />
-            </div>
+            <InputPair name='username' value='Username' type='text' />
+            <InputPair name='password' value='Password' type='password' />
 
             {/*TODO change onclick function to new login function*/}
-            <div class="center_login_buttons">
-                <button id="login_button" onclick="attemptLogin();" class="submit_button">Login</button>
-            </div>
+            <LoginButton id='login_button' /*onclick={() => attemptLogin()}*/ value='Log In' />
 
             <h1>Create Account</h1>
-            <div class="input_pair">
-                <label for="username">Username</label>
-                <input type="text" name="create_username" id="create_username" class="inputs" />
-            </div>
-
-            <div class="input_pair">
-                <label for="password">Password</label>
-                <input type="password" name="create_password" id="create_password" class="inputs"
-                    pattern=".{8,}" title="Eight or more characters" required />
-            </div>
+            <InputPair name='create_username' value='Username' type='text' />
+            <PasswordPair name='create_password' value='Password' />
 
             {/*TODO change onclick function to new add user function*/}
-            <div class="center_login_buttons">
-                <button id="add_user_button" onclick="addNewUser();" class="submit_button">Create</button>
-            </div>
+            <LoginButton id='add_user_button' /*onclick={() => addNewUser()}*/ value='Create' />
         </div>
     );
 }
