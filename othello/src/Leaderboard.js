0440e2614a74385247518fefe09140f472be6277
ElijahCParent <eparent@arizona.edu>
2023-11-16 22:19:18 -0700

    Added temp pages to be changed for each major page. Added router functionality in App.js. Fixed dependencies and added new imports.

---
+++ othello/src/Leaderboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Leaderboard() {
+    return (
+        <div>
+            <h1>Test Leaderboard Page</h1>
+        </div>
+    );
+}
+
+export default Leaderboard;
---
+++ othello/src/Replay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Replay() {
+    return (
+        <div>
+            <h1>Test Replay Page</h1>
+        </div>
+    );
+}
+
+export default Replay;
