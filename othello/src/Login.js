5613254107c953c1f7881aaa12030b6caffdecb7
ElijahCParent <eparent@arizona.edu>
2023-11-17 12:37:07 -0700

    Replacing class with className

--- othello/src/Login.js
+++ othello/src/Login.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function InputPair({id, value, type}) {
     return (
-        <div class='input_pair'>
+        <div className='input_pair'>
             <label for={id}>{value}</label>
-            <input type={type} name={id} id={id} class='inputs' pattern='.{8,}'
+            <input type={type} name={id} id={id} className='inputs' pattern='.{8,}'
                 title='Eight or more characters' required />
         </div>
     );
@@ -12,17 +12,17 @@ function InputPair({id, value, type}) {
 
 function PasswordPair({id, value}) {
     return (
-        <div class='input_pair'>
+        <div className='input_pair'>
             <label for={id}>{value}</label>
-            <input type='password' name={id} id={id} class='inputs' />
+            <input type='password' name={id} id={id} className='inputs' />
         </div>
     );
 }
 
 function LoginButton({id, onButtonClick, value}) {
     return (
-        <div class='center_login_buttons'>
-            <button id={id} onclick={onButtonClick} class='submit_button'>{value}</button>
+        <div className='center_login_buttons'>
+            <button id={id} onclick={onButtonClick} className='submit_button'>{value}</button>
         </div>
     );
 }
