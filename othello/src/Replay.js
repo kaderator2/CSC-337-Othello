e431a0df177fac1f6f779ada74e7cfaa847d5713
ElijahCParent <eparent@arizona.edu>
2023-11-18 16:43:41 -0700

    Outline of replay page complete.

--- othello/src/Replay.js
+++ othello/src/Replay.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
+import Default, {Header, BackButton, PlayerData} from './Components'
+
+function ForwardArrowButton({id}) {
+    return (
+        /*TODO make arrow onclick map to something*/
+        <div id={id} className='arrow_container' /*onClick=*/>
+            <img src={require('./images/arrow_right.png')} alt='A forward arrow symbol'
+                className='image_contain' width='50' />
+        </div>
+    );
+}
+function BackArrowButton({id}) {
+    return (
+        /*TODO make arrow onclick map to something*/
+        <div id={id} className='arrow_container' /*onClick=*/>
+            <img src={require('./images/arrow_left.png')} alt='A back arrow symbol'
+                className='image_contain' width='50' />
+        </div>
+    );
+}
 function Replay() {
     return (
         <div>
-            <h1>Test Replay Page</h1>
+            <BackButton />
+            <Header value='Replay' />
+            <div className='match_container centered_container'>
+                <div className='game_container centered_section'>
+                    <PlayerData id='top_player' name='test' rating='1400' />
+                    <PlayerData id='bottom_player' name='test2' rating='1450' />
+                    <div className="game">
+                        <p>Temp, game board location until implementation figured out</p>
+                    </div>
+                </div>
+                <ForwardArrowButton id='replay_forward' />
+                <BackArrowButton id='replay_back' />
+            </div>
         </div>
     );
 }
