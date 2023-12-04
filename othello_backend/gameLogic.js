d81e0304f459d66e792e8a55459885bfaf9b82b6
ElijahCParent <eparent@arizona.edu>
2023-12-03 17:36:41 -0700

    gameLogic is now commented

--- othello_backend/gameLogic.js
+++ othello_backend/gameLogic.js
@@ -1,12 +1,21 @@
 /**
- * This file contains the logic for the game.
- * It is responsible for creating new matches, adding board states to matches, and getting board states from matches.
+ * CSC 337 - Final Project - Elijah Parent, Kade Dean, Andres Silva-Castellanos
+ * This file contains the logic for the game. It is responsible for creating new matches, adding
+ * board states to matches, getting board states from matches, and otherwise updating match
+ * data.
  */
 
 const {User, Match, Board} = require('./schemas.js');
 
+/*
+This function creates a new match in the database, and returns the ID of the match as a string.
+
+req - an object representing an HTTP request.
+res - an object representing the response to an HTTP request.
+*/
 async function createMatch(req, res) {
     try {
+        //Set up initial board array
         const startingState = [
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -18,6 +27,7 @@ async function createMatch(req, res) {
             [0, 0, 0, 0, 0, 0, 0, 0]
         ];
 
+        //Create initial board
         const startingBoard = new Board({
             moveNumber: 0,
             nextPlayerTurn: 1,
@@ -26,7 +36,7 @@ async function createMatch(req, res) {
 
         await startingBoard.save();
 
-        //TODO get player names and ratings somehow
+        //Create match
         const match = new Match({
             player1Name: req.query.p1Username,
             player2Name: req.query.p2Username,
@@ -46,6 +56,12 @@ async function createMatch(req, res) {
     }
 }
 
+/*
+This function gets a match by ID, and returns its current data.
+
+req - an object representing an HTTP request.
+res - an object representing the response to an HTTP request.
+*/
 async function matchState(req, res) {
     try {
         let match = await Match.findOne({ _id: req.params.match_id }).exec();
@@ -60,6 +76,12 @@ async function matchState(req, res) {
     }
 }
 
+/*
+This function gets a board by ID, and returns its current data
+
+req - an object representing an HTTP request.
+res - an object representing the response to an HTTP request.
+*/
 async function boardState(req, res) {
     try {
         let board = await Board.findOne({ _id: req.params.board_id }).exec();
@@ -73,6 +95,13 @@ async function boardState(req, res) {
     }
 }
 
+/*
+This function creates a new board with data found in the request, and adds it to the provided match
+by ID. Both are then saved.
+
+req - an object representing an HTTP request.
+res - an object representing the response to an HTTP request.
+*/
 async function addBoardState(req, res) {
     try {
         var match = await Match.findOne({ _id: req.body.match }).exec();
@@ -97,6 +126,12 @@ async function addBoardState(req, res) {
     }
 }
 
+/*
+This function gets a match by ID, updates the winner, and saves it.
+
+req - an object representing an HTTP request.
+res - an object representing the response to an HTTP request.
+*/
 async function updateWinner(req, res) {
     try {
         var match = await Match.findOne({ _id: req.body.matchID }).exec();
@@ -112,6 +147,12 @@ async function updateWinner(req, res) {
     }
 }
 
+/*
+This function gets a player by name, updates their rating, and saves it.
+
+req - an object representing an HTTP request.
+res - an object representing the response to an HTTP request.
+*/
 async function updateRating(req, res) {
     try {
         var user = await User.findOne({ username: req.body.username }).exec();
